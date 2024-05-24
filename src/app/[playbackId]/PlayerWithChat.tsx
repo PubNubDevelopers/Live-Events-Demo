19fa856d973e63e910d0d6570b41e21762fa15c4
markus.kohler <markus.kohler@pubnub.com>
2024-05-24 15:30:24 -0400

    Player fix

--- src/app/[playbackId]/PlayerWithChat.tsx
+++ src/app/[playbackId]/PlayerWithChat.tsx
@@ -1,5 +1,5 @@
 import type { Src } from "@livepeer/react";
-import { PlayerWithControls } from "../../../components/player/Player";
+import { PlayerWithControls } from "../../components/player/Player";
 
 export default function PlayerWithChat({ src }: { src: Src[] }) {
   return <PlayerWithControls src={src} />;
